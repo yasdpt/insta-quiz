c6d409aead486d454e0a4739e85094e5c7a2b185
MrYaS <mryas3352@gmail.com>
2023-10-05 11:17:28 +0300

    fix: update sendGameDataToUser

--- server/src/util/bot-util.ts
+++ server/src/util/bot-util.ts
@@ -1,33 +1,35 @@
 import { Markup, Telegraf } from "telegraf";
 import pool from "./pool";
 
-const bot = new Telegraf(process.env.BOT_TOKEN ?? "");
-
 const sendGameDataToUser = async (
   webAppQuery: string,
   gameId: number,
   categoryId: number
 ) => {
-  const catRes = await pool.query("SELECT * FROM categories WHERE id = $1", [
-    categoryId,
-  ]);
-
-  const categoryName = catRes.rows[0].name;
-  bot.telegram.answerWebAppQuery(webAppQuery, {
-    id: "0",
-    type: "article",
-    title: "Your game was created!",
-    input_message_content: {
-      message_text: `A game in the category "${categoryName}" was created, send this message for your friends to join the game!`,
-    },
-    ...Markup.inlineKeyboard([
-      // WEB_APP_URL is direct link of your website, not web app's bot link.
-      Markup.button.webApp(
-        "Join game",
-        `${process.env.WEB_APP_URL || ""}/?joinGame=${gameId}`
-      ),
-    ]),
-  });
+  try {
+    const catRes = await pool.query("SELECT * FROM categories WHERE id = $1", [
+      categoryId,
+    ]);
+    const bot = new Telegraf(process.env.BOT_TOKEN ?? "");
+    const categoryName = catRes.rows[0].name;
+    await bot.telegram.answerWebAppQuery(webAppQuery, {
+      id: "0",
+      type: "article",
+      title: "Your game was created!",
+      input_message_content: {
+        message_text: `A game in the category "${categoryName}" was created, send this message for your friends to join the game!`,
+      },
+      ...Markup.inlineKeyboard([
+        // WEB_APP_URL is direct link of your website, not web app's bot link.
+        Markup.button.webApp(
+          "Join game",
+          `${process.env.WEB_APP_URL || ""}/join?game=${gameId}`
+        ),
+      ]),
+    });
+  } catch (error) {
+    console.log(`Send bot message error: ${error}`);
+  }
 };
 
 export default sendGameDataToUser;
