4b2fd6d343096969e2dead718e8cb2744ce34bf7
MrYaS <mryas3352@gmail.com>
2023-10-05 11:46:47 +0300

    feat: add getWailList and exception handling

--- server/src/util/game-util.ts
+++ server/src/util/game-util.ts
@@ -2,21 +2,25 @@ import { ScoreboardUser } from "../models/scoreboard";
 import pool from "./pool";
 import myCache from "./cache";
 import { GameInfo } from "../models/game";
+import { WaitListUser } from "../models/user";
 
-// Get game info and the current question and it's answers
+// Get game info and it's questions and their answers
+// With current asked question returned in object
+// Cache can also be used with fromCache = true
 const getCurrentGameInfo = async (
   gameId: number,
   fromCache: boolean = false
 ): Promise<GameInfo | undefined> => {
-  if (fromCache) {
-    const gameInfo = myCache.get<string>(gameId.toString());
-    if (gameInfo) {
-      return JSON.parse(gameInfo);
+  try {
+    if (fromCache) {
+      const gameInfo = myCache.get<string>(gameId.toString());
+      if (gameInfo) {
+        return JSON.parse(gameInfo);
+      }
     }
-  }
 
-  const gameResult = await pool.query(
-    `
+    const gameResult = await pool.query(
+      `
     SELECT
       g.id, 
       g.owner_id,
@@ -35,110 +39,145 @@ const getCurrentGameInfo = async (
     LEFT JOIN answers AS a ON a.question_id = q.id
     WHERE g.id = $1;
     `,
-    [gameId]
-  );
+      [gameId]
+    );
 
-  if (gameResult.rowCount > 0) {
-    const game: GameInfo = {
-      id: gameResult.rows[0].id,
-      owner_id: gameResult.rows[0].owner_id,
-      category_id: gameResult.rows[0].category_id,
-      status: gameResult.rows[0].status,
-      last_question_time: gameResult.rows[0].last_question_time,
-      current_question: gameResult.rows[0].current_question,
-      questions: [],
-    };
+    if (gameResult.rowCount > 0) {
+      const game: GameInfo = {
+        id: gameResult.rows[0].id,
+        owner_id: gameResult.rows[0].owner_id,
+        category_id: gameResult.rows[0].category_id,
+        status: gameResult.rows[0].status,
+        last_question_time: gameResult.rows[0].last_question_time,
+        current_question: gameResult.rows[0].current_question,
+        questions: [],
+      };
 
-    // Map to track questions by id
-    const questionsMap = new Map();
+      // Map to track questions by id
+      const questionsMap = new Map();
 
-    for (const row of gameResult.rows) {
-      // See if question already exists
-      let question = questionsMap.get(row.question_id);
+      for (const row of gameResult.rows) {
+        // See if question already exists
+        let question = questionsMap.get(row.question_id);
 
-      if (!question) {
-        // Add new question object
-        question = {
-          id: row.question_id,
-          text: row.question,
-          answers: [],
-        };
+        if (!question) {
+          // Add new question object
+          question = {
+            id: row.question_id,
+            text: row.question,
+            answers: [],
+          };
 
-        // Save to map
-        questionsMap.set(row.question_id, question);
+          // Save to map
+          questionsMap.set(row.question_id, question);
 
-        // Also add to game questions array
-        game.questions.push(question);
+          // Also add to game questions array
+          game.questions.push(question);
+        }
+
+        // Now we can directly access the question object
+        question.answers.push({
+          id: row.answer_id,
+          text: row.answer,
+          is_correct: row.is_correct,
+        });
       }
 
-      // Now we can directly access the question object
-      question.answers.push({
-        id: row.answer_id,
-        text: row.answer,
-        is_correct: row.is_correct,
-      });
+      return game;
     }
-
-    return game;
+  } catch (error) {
+    console.log(`Get gameInfo error: ${error}`);
   }
 };
 
+// Get list of users in game with their scores
+// Cache can also be used with fromCache = true
 const getLeaderboardInfo = async (
   gameId: number,
   fromCache: boolean = false
 ): Promise<ScoreboardUser[] | undefined> => {
-  if (fromCache) {
-    const leaderboard = myCache.get<string>(`${gameId}-leaderboard`);
-    if (leaderboard) {
-      return JSON.parse(leaderboard);
+  try {
+    if (fromCache) {
+      const leaderboard = myCache.get<string>(`${gameId}-leaderboard`);
+      if (leaderboard) {
+        return JSON.parse(leaderboard);
+      }
     }
-  }
 
-  const sbResult = await pool.query(
-    `
-    SELECT
-      u.id AS user_id,
-      u.first_name,
-      u.last_name,
-      gu.score,
-      gq.question_id AS question_id,
-      a.id AS answer_id,
-      a.is_correct
-    FROM game_questions AS gq 
-    LEFT JOIN game_users AS gu ON gq.game_id = gu.game_id
-    LEFT JOIN users AS u ON gu.user_id = u.id
-    LEFT JOIN game_user_answers AS gua ON gu.user_id = gua.user_id AND gq.question_id = gua.question_id
-    LEFT JOIN answers AS a ON gua.answer_id = a.id
-    WHERE gq.game_id = $1;
-    `,
-    [gameId]
-  );
+    const sbResult = await pool.query(
+      `
+      SELECT
+        u.id AS user_id,
+        u.first_name,
+        u.last_name,
+        gu.score,
+        gq.question_id AS question_id,
+        a.id AS answer_id,
+        a.is_correct
+      FROM game_questions AS gq 
+      LEFT JOIN game_users AS gu ON gq.game_id = gu.game_id
+      LEFT JOIN users AS u ON gu.user_id = u.id
+      LEFT JOIN game_user_answers AS gua ON gu.user_id = gua.user_id AND gq.question_id = gua.question_id
+      LEFT JOIN answers AS a ON gua.answer_id = a.id
+      WHERE gq.game_id = $1;
+      `,
+      [gameId]
+    );
 
-  const users = new Map<number, ScoreboardUser>();
+    const users = new Map<number, ScoreboardUser>();
 
-  for (const row of sbResult.rows) {
-    const userId = row.user_id;
+    for (const row of sbResult.rows) {
+      const userId = row.user_id;
 
-    if (!users.has(userId)) {
-      users.set(userId, {
-        user_id: userId,
-        first_name: row.first_name,
-        last_name: row.last_name,
-        score: row.score,
-        answers: [],
+      if (!users.has(userId)) {
+        users.set(userId, {
+          user_id: userId,
+          first_name: row.first_name,
+          last_name: row.last_name,
+          score: row.score,
+          answers: [],
+        });
+      }
+
+      users.get(userId)?.answers.push({
+        question_id: row.question_id,
+        answer_id: row.answer_id,
+        is_correct: row.is_correct,
       });
     }
 
-    users.get(userId)?.answers.push({
-      question_id: row.question_id,
-      answer_id: row.answer_id,
-      is_correct: row.is_correct,
-    });
+    const leaderboard = Array.from(users.values());
+
+    return leaderboard;
+  } catch (error) {
+    console.log(`Get leaderboard error: ${error}`);
   }
-
-  const leaderboard = Array.from(users.values());
-
-  return leaderboard;
 };
 
-export { getCurrentGameInfo, getLeaderboardInfo };
+// Get a list of users signed up for the game
+const getWailList = async (
+  gameId: number
+): Promise<WaitListUser[] | undefined> => {
+  try {
+    const wlResult = await pool.query(
+      `
+      SELECT
+        u.id AS user_id,
+        u.first_name,
+        u.last_name,
+        u.username
+      FROM games as g
+      LEFT JOIN game_users AS gu ON g.id = gu.game_id
+      LEFT JOIN users AS u ON gu.user_id = u.id
+      WHERE g.id = $1;
+      `,
+      [gameId]
+    );
+
+    return wlResult.rows;
+  } catch (error) {
+    console.log(`getWaitList error: ${error}`);
+  }
+};
+
+export { getCurrentGameInfo, getLeaderboardInfo, getWailList };
