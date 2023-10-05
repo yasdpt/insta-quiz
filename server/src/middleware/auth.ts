fae25c348da33e97c006d54727cd203e6d272803
MrYaS <mryas3352@gmail.com>
2023-10-05 23:21:36 +0300

    feat: add socket token auth

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,17 +1,60 @@
 import { Request, Response, NextFunction } from "express";
 import { createHmac } from "node:crypto";
+import { Socket } from "socket.io";
 
+// Verify token from express middleware
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-  const authorization = req.get("Authorization");
-  const telegramInitData = authorization?.split(" ")[1] ?? "";
+  try {
+    const authorization = req.get("Authorization");
+    const telegramInitData = authorization?.split(" ")[1] ?? "";
 
-  if (!telegramInitData) {
-    return res.status(401).json({ message: "No token provided" });
+    if (!telegramInitData) {
+      return res.status(401).json({ message: "No token provided" });
+    }
+
+    // if hash are equal the data may be used on your server.
+    // Complex data types are represented as JSON-serialized objects.
+    if (hashValidated(telegramInitData)) {
+      next();
+    } else {
+      return res.status(401).json({ message: "Authentication failed!" });
+    }
+  } catch (error) {
+    console.log(`Auth error: ${error}`);
+    return res.status(500).json({ message: "Authentication error!" });
   }
+};
+
+// Verify token from socket middleware
+const verifyTokenSocket = (socket: Socket, next: any) => {
+  try {
+    const authorization = socket.handshake.headers.authorization ?? "";
+    const telegramInitData = authorization?.split(" ")[1] ?? "";
+
+    if (!telegramInitData) {
+      next(new Error("No token provided"));
+      return;
+    }
+
+    // if hash are equal the data may be used on your server.
+    // Complex data types are represented as JSON-serialized objects.
+    if (hashValidated(telegramInitData)) {
+      next();
+    } else {
+      next(new Error("Authentication failed!"));
+    }
+  } catch (error) {
+    console.log(`Socket auth failed: ${error}`);
+    next(new Error("Error happened"));
+  }
+};
+
+// Check validation of telegram init data
+const hashValidated = (initData: string): boolean => {
   const botToken = process.env.BOT_TOKEN ?? "";
 
   // The data is a query string, which is composed of a series of field-value pairs.
-  const encoded = decodeURIComponent(telegramInitData);
+  const encoded = decodeURIComponent(initData);
 
   // HMAC-SHA-256 signature of the bot's token with the constant string WebAppData used as a key.
   const secret = createHmac("sha256", "WebAppData").update(botToken);
@@ -31,13 +74,7 @@ const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     .update(dataCheckString)
     .digest("hex");
 
-  // if hash are equal the data may be used on your server.
-  // Complex data types are represented as JSON-serialized objects.
-  if (_hash === hash) {
-    next();
-  } else {
-    return res.status(401).json({ message: "Authentication failed!" });
-  }
+  return _hash === hash;
 };
 
-export default verifyToken;
+export { verifyToken, verifyTokenSocket };
