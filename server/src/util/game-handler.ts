4d9f0b5ded7e31d4838e56c3521b699412b3e7a9
MrYaS <mryas3352@gmail.com>
2023-10-05 23:19:40 +0300

    chore: remove update leaderboard in game loop

--- server/src/util/game-handler.ts
+++ server/src/util/game-handler.ts
@@ -4,7 +4,7 @@ import myCache from "./cache";
 import { getCurrentGameInfo, getLeaderboardInfo } from "./game-util";
 import pool from "./pool";
 
-const roundDuration = 10000; // in miliseconds
+const roundDuration = 15000; // in miliseconds
 const totalQuestions = 10; // number of questions
 const cacheTTL = 30;
 
@@ -16,50 +16,52 @@ const gameLoop = async (
   let currentQuestionIndex = 0;
 
   while (currentQuestionIndex <= totalQuestions) {
-    // Question timeout
-    await new Promise((resolve) => setTimeout(resolve, roundDuration));
+    try {
+      // Question timeout
+      await new Promise((resolve) => setTimeout(resolve, roundDuration));
 
-    // Update leaderboard cache and broadcast to users
-    getLeaderboardInfo(gameId).then((leaderboardInfo): any => {
-      myCache.set(
-        `${gameId}-leaderboard`,
-        JSON.stringify(leaderboardInfo),
-        cacheTTL
-      );
-      updateLeaderboard(gameId, true);
-    });
+      const gameInfo = (await getCurrentGameInfo(gameId, true)) as GameInfo;
 
-    const gameInfo = (await getCurrentGameInfo(gameId, true)) as GameInfo;
+      // Update current question index
+      currentQuestionIndex += 1;
 
-    // Update current question index
-    currentQuestionIndex += 1;
+      // When index is 10 means last question was asked and
+      // a new question will not be sent and the game will end
+      if (currentQuestionIndex < 10) {
+        // Change question and time started
+        const questionAskTime = moment().utc().format();
+        gameInfo.current_question = gameInfo.questions[currentQuestionIndex].id;
+        gameInfo.question_index = currentQuestionIndex;
+        gameInfo.last_question_time = questionAskTime;
 
-    // When index is 10 means last question was asked and
-    // a new question will not be sent and the game will end
-    if (currentQuestionIndex < 10) {
-      // Change question and time started
-      const questionAskTime = moment().utc().format();
-      gameInfo.current_question = gameInfo.questions[currentQuestionIndex].id;
-      gameInfo.last_question_time = questionAskTime;
+        console.log(
+          `Question: ${
+            gameInfo.current_question
+          }, Ask time: ${questionAskTime.toString()}`
+        );
 
-      // Update game info in db
-      pool.query(
-        "UPDATE games SET current_question = $1, last_question_time = $2 WHERE id = $3",
-        [gameInfo.current_question, questionAskTime, gameId]
-      );
-    } else {
-      // Set game status to ended
-      gameInfo.status = 2;
+        // Update game info in db
+        pool.query(
+          "UPDATE games SET current_question = $1, last_question_time = $2 WHERE id = $3",
+          [gameInfo.current_question, questionAskTime, gameId]
+        );
+      } else {
+        // Set game status to ended
+        gameInfo.status = 2;
 
-      // End game info in db
-      pool.query("UPDATE games SET status = 2, WHERE id = $1", [gameId]);
+        // End game info in db
+        pool.query("UPDATE games SET status = 2 WHERE id = $1", [gameId]);
+      }
+
+      // Update gameInfo in cache
+      myCache.set(gameId.toString(), JSON.stringify(gameInfo), cacheTTL);
+
+      // Broadcast game info to all users
+      updateGame(gameId, true);
+    } catch (error) {
+      console.log(`Game loop error: ${error}`);
+      continue;
     }
-
-    // Update gameInfo in cache
-    myCache.set(gameId.toString(), JSON.stringify(gameInfo), cacheTTL);
-
-    // Broadcast game info to all users
-    updateGame(gameId, true);
   }
 };
 
