0a9778db35c98f83c96c8b10e5bf92fbaccf11b9
MrYaS <mryas3352@gmail.com>
2023-10-05 11:47:24 +0300

    chore: add auth middleware to users route

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import verifyToken from "../middleware/auth";
+import { verifyToken } from "../middleware/auth";
 import pool from "../util/pool";
 import { User } from "../models/user";
 
