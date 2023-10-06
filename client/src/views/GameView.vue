a788fc9bdf5250ce7d879df03dd37f2b1216bf14
MrYaS <mryas3352@gmail.com>
2023-10-06 20:03:03 +0300

    fix: transition only one child error

--- client/src/views/GameView.vue
+++ client/src/views/GameView.vue
@@ -1,27 +1,28 @@
 <template>
   <div class="relative container mx-auto">
-    <div
-      class="bg-tgBackground flex flex-col justify-start h-screen p-3 mx-auto"
-    >
-      <Transition name="card">
+    <Transition name="card">
+      <div
+        class="bg-tgBackground flex flex-col justify-start h-screen p-3 mx-auto"
+      >
         <GameQuestion v-if="store.gameIsRunning" />
-        <GameInfo v-if="!store.gameIsRunning" />
-      </Transition>
 
-      <p class="text-tgText text-xl text-center font-semibold mt-8">
-        Leaderboard
-      </p>
-      <TransitionGroup name="list" tag="GameScore">
-        <GameScore
-          class="mx-2 md:mx-auto md:w-96"
-          v-for="(user, index) in store.leaderboard"
-          :key="index"
-          :name="`${user.first_name} ${user.last_name}`"
-          :score="user.score"
-          :user="user"
-        />
-      </TransitionGroup>
-    </div>
+        <GameInfo v-if="!store.gameIsRunning" />
+
+        <p class="text-tgText text-xl text-center font-semibold mt-8">
+          Leaderboard
+        </p>
+        <TransitionGroup name="list" tag="GameScore">
+          <GameScore
+            class="mx-2 md:mx-auto md:w-96"
+            v-for="(user, index) in store.leaderboard"
+            :key="index"
+            :name="`${user.first_name} ${user.last_name}`"
+            :score="user.score"
+            :user="user"
+          />
+        </TransitionGroup>
+      </div>
+    </Transition>
   </div>
 </template>
 
